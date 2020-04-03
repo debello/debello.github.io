4993f234b9921719694bc86ac8a467bf0b9dd59a
debello <42223093+debello@users.noreply.github.com>
2020-04-03 20:31:56 +0200

    Updates

---
+++ precache-manifest.ca42ca4b863052698fe2221884c79e1a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1306fd0165d09a36d56859e23dacb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5c5f89c8568d0b9c1b1",
+    "url": "/static/js/2.b566547e.chunk.js"
+  },
+  {
+    "revision": "9f55a1ac102148e41f3a9543b31fc30f",
+    "url": "/static/js/2.b566547e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ceba16db63070df288",
+    "url": "/static/js/main.4d0762e6.chunk.js"
+  },
+  {
+    "revision": "15a88e3a38da53c2fc3e",
+    "url": "/static/js/runtime-main.8614b118.js"
+  },
+  {
+    "revision": "b608deeb954ed518eafc31d1c7553540",
+    "url": "/static/media/DeterminationSansWeb.b608deeb.ttf"
+  },
+  {
+    "revision": "6978d81cb30ee9c6598c6e69d43b4993",
+    "url": "/static/media/EndlessBossBattle.6978d81c.ttf"
+  },
+  {
+    "revision": "0227344c7a4fcab5ed78b9c4b193c5bb",
+    "url": "/static/media/corazon-amarillo.0227344c.png"
+  },
+  {
+    "revision": "b9156a8114504b2751cd397cd5d78e87",
+    "url": "/static/media/corazon-azul.b9156a81.png"
+  },
+  {
+    "revision": "fa459ea00cfded14d8094d3db2426b04",
+    "url": "/static/media/corazon-rojo.fa459ea0.png"
+  },
+  {
+    "revision": "f0e6a9daf2f9df2e61e6652780f86818",
+    "url": "/static/media/corazon-verde.f0e6a9da.png"
+  },
+  {
+    "revision": "b52e95c120cefccc573f555ee444e3ca",
+    "url": "/static/media/corazon-violeta.b52e95c1.png"
+  },
+  {
+    "revision": "427b8bc29df2dae700937053281fcaa7",
+    "url": "/static/media/icono-github.427b8bc2.png"
+  },
+  {
+    "revision": "4b514fbf346f8363eed03955504c0a93",
+    "url": "/static/media/icono-twitter.4b514fbf.png"
+  }
+]);
