241bec8679a19b1b43f5c024fecdc68001e157ad
debello <42223093+debello@users.noreply.github.com>
2020-04-03 20:36:32 +0200

    Updates

---
+++ precache-manifest.eb085a6ff04c61d7aa4a7b3bb1a08284.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268fe6766a21f31106bded6865b07e9a",
+    "url": "/debello.github.io/index.html"
+  },
+  {
+    "revision": "ebab80a1ffb244e8aa14",
+    "url": "/debello.github.io/static/js/2.26282c9f.chunk.js"
+  },
+  {
+    "revision": "9f55a1ac102148e41f3a9543b31fc30f",
+    "url": "/debello.github.io/static/js/2.26282c9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce212cd7640518305b5",
+    "url": "/debello.github.io/static/js/main.e70718b5.chunk.js"
+  },
+  {
+    "revision": "d60ca08c9b21decfab1f",
+    "url": "/debello.github.io/static/js/runtime-main.0e4e069a.js"
+  },
+  {
+    "revision": "b608deeb954ed518eafc31d1c7553540",
+    "url": "/debello.github.io/static/media/DeterminationSansWeb.b608deeb.ttf"
+  },
+  {
+    "revision": "6978d81cb30ee9c6598c6e69d43b4993",
+    "url": "/debello.github.io/static/media/EndlessBossBattle.6978d81c.ttf"
+  },
+  {
+    "revision": "0227344c7a4fcab5ed78b9c4b193c5bb",
+    "url": "/debello.github.io/static/media/corazon-amarillo.0227344c.png"
+  },
+  {
+    "revision": "b9156a8114504b2751cd397cd5d78e87",
+    "url": "/debello.github.io/static/media/corazon-azul.b9156a81.png"
+  },
+  {
+    "revision": "fa459ea00cfded14d8094d3db2426b04",
+    "url": "/debello.github.io/static/media/corazon-rojo.fa459ea0.png"
+  },
+  {
+    "revision": "f0e6a9daf2f9df2e61e6652780f86818",
+    "url": "/debello.github.io/static/media/corazon-verde.f0e6a9da.png"
+  },
+  {
+    "revision": "b52e95c120cefccc573f555ee444e3ca",
+    "url": "/debello.github.io/static/media/corazon-violeta.b52e95c1.png"
+  },
+  {
+    "revision": "427b8bc29df2dae700937053281fcaa7",
+    "url": "/debello.github.io/static/media/icono-github.427b8bc2.png"
+  },
+  {
+    "revision": "4b514fbf346f8363eed03955504c0a93",
+    "url": "/debello.github.io/static/media/icono-twitter.4b514fbf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca42ca4b863052698fe2221884c79e1a.js"
+  "/debello.github.io/precache-manifest.eb085a6ff04c61d7aa4a7b3bb1a08284.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/debello.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
